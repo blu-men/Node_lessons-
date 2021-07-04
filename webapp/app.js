7d31fdb4ce852f896da05dec3fdd060c52a3e057
blu-men <cjbb5eyv@icloud.com>
2021-07-04 23:34:51 +0900

    begin_logging

--- webapp/app.js
+++ webapp/app.js
@@ -12,8 +12,10 @@
 
 var http, server;
 
+// httpモジュールを変数httpに格納
 http   = require( 'http' );
 server = http.createServer( function ( request, response ) {
+  console.log(request);
   response.writeHead( 200, { 'Content-Type': 'text/plain' } );
   response.end( 'Hello World' );
 }).listen( 3000 );
